303b30a86ff6f3316734c27a694a169553bcfa09
Ilya <sok203333@gmail.com>
2024-08-04 20:35:52 +0300

    copied code from yandex docs for search provider

--- main_scripts.js
+++ main_scripts.js
@@ -1,10 +1,6 @@
 "use strict";
 
 addEventListener("resize", (event) => {
-    console.log(`
-    Внешняя высота: ${outerHeight};
-    Внешняя ширина: ${outerWidth}`
-    );
     const frame = document.getElementById("main_frame");
     frame.style.height = `${outerHeight/2}px`;
     frame.style.width = `${outerWidth/2}px`;
